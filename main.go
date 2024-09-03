0a19cf1bdcbb6b6d58b63e02dbf2b34fb77252e3
Irwan Setiawan <irwan@irwansetiawan.com>
2024-09-04 01:09:56 +0800

    Enable CORS

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	apiv0 "ai-audience-analytics/api/v0"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
 
+	app.Use(cors.New())
 	apiv0.SetupRoutes(app)
 
 	app.Listen(":3000")
