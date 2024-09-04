6219dd07d668fca865d90231916d17d8727ebaee
Irwan Setiawan <irwan@irwansetiawan.com>
2024-09-05 00:07:39 +0800

    Update app port and show error when port is in use

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	apiv0 "ai-audience-analytics/api/v0"
+	"fmt"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -13,5 +14,11 @@ func main() {
 	app.Use(cors.New())
 	apiv0.SetupRoutes(app)
 
-	app.Listen(":3000")
+	appPort := 8080
+	appErr := app.Listen(fmt.Sprintf(":%d", appPort))
+	if appErr != nil {
+		panic(appErr)
+	} else {
+		println(fmt.Sprintf("Server is running on port %d", appPort))
+	}
 }
